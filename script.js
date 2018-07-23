017e94ef2fde6f67f284b4afe24d3fcc8941ed25
Christopher Culp <Christopher.D.Culp@gmail.com>
2018-07-23 07:44:21 -0400

    First commit, basic math added

---
+++ script.js
@@ -0,0 +1,37 @@
+let state = "";
+let mem = "";
+let operation = "add";
+
+function add(a, b) {
+    state = a + b;
+}
+
+function sub(a, b) {
+    state = a - b;
+}
+
+function div(a, b) {
+    state = a / b;
+}
+
+function mult(a, b) {
+    state = a * b;
+}
+
+function operate(op) {
+    switch (op) {
+        case "add":
+            console.log("adding");
+            add(parseFloat(mem), parseFloat(state));
+            break;
+        case "sub":
+            sub(parseFloat(mem), parseFloat(state));
+            break;
+        case "div":
+            div(parseFloat(mem), parseFloat(state));
+            break;
+        case "mult":
+            mult(parseFloat(mem), parseFloat(state));
+            break;
+    }
+}
